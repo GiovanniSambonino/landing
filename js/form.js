3941a58bae4a57ab18f0450898eb53dbc5e9a47d
Giovanni Sambonino <87047313+GiovanniSambonino@users.noreply.github.com>
2024-06-10 14:42:07 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,9 +1,17 @@
 const formulario = document.getElementById('formulario');
 formulario.addEventListener('submit', (event) => {
     event.preventDefault();
+
+    if(email.value.length==0){
+        alert("email requerido")
+        email.focus()
+        return;
+    }
     const email = document.getElementById('email').value;
+    const product = document.getElementById('product').value;
     const datos = {
-        email: email
+        email: email,
+        product: product
     };
     fetch('https://databasedawm-default-rtdb.firebaseio.com/collection.json', {
         method: 'POST',
@@ -20,3 +28,27 @@ formulario.addEventListener('submit', (event) => {
         })
         .catch(error => console.error(error));
 });
+
+async function obtenerDatos() {
+    const url = "https://databasedawm-default-rtdb.firebaseio.com/collection.json"; // Reemplaza con la URL real de la API o recurso
+    const respuesta = await fetch(url);
+    if (!respuesta.ok) {
+    console.error("Error:", respuesta.status);
+    return;
+    }
+    const datos = await respuesta.json();
+    console.log(datos); // Procesar o mostrar los datos obtenidos
+    const tablaBody = document.getElementById('tablebody');
+    Object.keys(datos).forEach(key => {
+        const { product, conteo } = datos[key];
+        let fila = document.createElement('tr');
+        fila.innerHTML = `
+            <td>${product}</td>
+            <td>${conteo}</td>
+        `;
+        tablaBody.appendChild(fila);
+    });
+    }
+    obtenerDatos();
+
+    
