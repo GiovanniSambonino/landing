a5ff8ba38bf16c475ce830f6d55883c4b0adc853
Giovanni Sambonino <87047313+GiovanniSambonino@users.noreply.github.com>
2024-06-16 14:35:47 -0500

    Upadte

--- js/form.js
+++ js/form.js
@@ -2,7 +2,7 @@ const formulario = document.getElementById('formulario');
 formulario.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    if(email.value.length==0){
+    if (email.value.length == 0) {
         alert("email requerido")
         email.focus()
         return;
@@ -33,22 +33,45 @@ async function obtenerDatos() {
     const url = "https://databasedawm-default-rtdb.firebaseio.com/collection.json"; // Reemplaza con la URL real de la API o recurso
     const respuesta = await fetch(url);
     if (!respuesta.ok) {
-    console.error("Error:", respuesta.status);
-    return;
+        console.error("Error:", respuesta.status);
+        return;
     }
     const datos = await respuesta.json();
-    console.log(datos); // Procesar o mostrar los datos obtenidos
-    const tablaBody = document.getElementById('tablebody');
-    Object.keys(datos).forEach(key => {
-        const { product, conteo } = datos[key];
-        let fila = document.createElement('tr');
-        fila.innerHTML = `
-            <td>${product}</td>
-            <td>${conteo}</td>
-        `;
-        tablaBody.appendChild(fila);
-    });
-    }
-    obtenerDatos();
 
-    
+    let votesMap = new Map();
+    for (const key in datos) {
+        let vote = datos[key]
+        let producto = vote['product']
+
+        let conteo = votesMap.has(producto) ? votesMap.get(producto) + 1 : 1;
+        votesMap.set(producto, conteo)
+    }
+
+    total = 0
+    tablebody.innerHTML = ''
+
+    console.log(votesMap)
+
+    for (let key of votesMap.keys()) {
+        template += `
+        <tr>
+            <td>${key}</td>
+            <td>${votesMap.get(key)}</td>
+        </tr>
+        `
+        tablebody.innerHTML += template
+
+        total += votesMap.get(key)
+    }
+    tablebody.innerHTML +=`
+    <tr>
+        <td class="text-black font-weight-bold"><strong>Productos<strong></td>
+        <td class="text-black font-weight-bold"><strong>${total}<strong></td>
+    </tr>
+    `
+}
+obtenerDatos();
+
+
+
+
